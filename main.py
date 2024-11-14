da2e7419975216f9675955d924e0ae287d775dc4
Rohan Patel <rohanptl05@gmail.com>
2024-11-14 21:43:23 +0530

    commit1

--- main.py
+++ main.py
@@ -7,8 +7,9 @@ while (a !=n):
     a = int(input("Guess a number between 1 and 100 : "))
     if(a>n):
         print("Lower Number please ..")
+        guesses += 1
     elif(a<n):
         print("Higher Number please ..")    
-    guesses += 1
+        guesses += 1
 
 print(f"You have correct Number is {n} currectly {guesses} attemps")
